8eb08d0a0af73d42eb0da1d5f24603df3554c4b6
Edmund Teo Chang Hao <ainurlindale@gmail.com>
2022-10-15 09:05:40 +0800

    Initial Setup, plus center align

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import ProfileCard from "./Components/ProfileCard";
+
+function App() {
+  return <ProfileCard />;
+}
+
+export default App;
---
+++ src/Components/ProfileCard.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Unstable_Grid2";
+
+export default function ProfileCard() {
+  return (
+    <div>
+      <Container>
+        <Grid
+          xs
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          minHeight={"100vh"}
+          style={{ border: "1px solid red" }}
+        >
+          <h1> asdsad</h1>
+        </Grid>
+      </Container>
+    </div>
+  );
+}
